0e529637964bd7cea75240f49e764b3761cb6017
mattbfishman <matthewbrandonfishman@gmail.com>
2020-03-26 23:08:01 -0400

    Update package lock

--- src/App.js
+++ src/App.js
@@ -4,19 +4,9 @@ import Table from 'fish-ui-ac/dist/Table/Table';
 
 class App extends Component {
     render(){
-        var rowData = [
-            ["Dab","300","Fish"  ],
-            ["Olive Flounder","800","Fish"  ],
-            ["Red Snapper","3000","Fish"  ],
-            ["Sea Butterfly","1000","Fish"  ],
-            ["Carp","300","Fish"  ],
-            ["Horse Mackerel","150","Fish"  ],
-            ["Bitterling","900","Fish"  ],
-            ["Loach","400","Fish"  ]];
         return( 
             <div>
                 <Search/>
-                <Table rows={rowData}/>
             </div>
         );
     }
