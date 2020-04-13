52518296a5871c7823a236611153d8b2a1868de0
mattbfishman <matthewbrandonfishman@gmail.com>
2020-04-12 23:11:39 -0400

    update fish-ui version, add basic nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import CardWrapper from './components/CardWrapper';
 import SearchWrapper from './components/SearchWrapper';
 import FilterWrapper from './components/FilterWrapper';
+import Navbar from 'fish-ui-ac/dist/Navbar/Navbar';
+import {navItems} from './configs/nav';
+import navLogo from './images/navlogo.png';
+
 import './app.scss';
 
 class App extends Component {
@@ -9,6 +13,7 @@ class App extends Component {
     render(){
         return( 
             <div>
+                <Navbar logo={navLogo} navItems={navItems} />
                 <SearchWrapper/>
                 <FilterWrapper/>
                 <CardWrapper/>
